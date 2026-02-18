1718b451b791004ed0f00eff81c4ca316d5ae967
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2026-02-18 15:27:19 +0530

    added utility functions to generate and execute python files

---
+++ server/utils/pythonCompiler.js
@@ -0,0 +1,31 @@
+const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuid } = require('uuid');
+
+const dirCodes = path.join(__dirname, "../python_runner");
+if (!fs.existsSync(dirCodes)) {
+    fs.mkdirSync(dirCodes, { recursive: true });
+}
+
+const generatefile = async (format, content) => {
+    const jobId = uuid(); 
+    const filename = `${jobId}.${format}`;
+    const filepath = path.join(dirCodes, filename);
+    fs.writeFileSync(filepath, content);
+    return filepath;
+};
+
+const executepy = (filepath) => {
+    return new Promise((resolve, reject) => {
+        const uniqueName = path.basename(filepath).split(".")[0];
+        const wayName = path.join(__dirname, "../python_runner");
+        exec(`cd ${wayName} && python3 ${uniqueName}.py`, (error, stdout, stderr) => {
+            if (error) reject(error);
+            else if (stderr) reject(stderr);
+            else resolve(stdout);
+        });
+    });
+};
+
+module.exports = { executepy, generatefile };

d6aac7e5d0d376751495af70fc792f37d75fe141
Shalini0810 <shalinikotha08@gmail.com>
2026-02-18 20:11:37 +0530

    added placeholder for javascript.jsx before writing the code

--- client/src/components/Javascript.jsx
+++ client/src/components/Javascript.jsx
@@ -75,20 +75,6 @@ useEffect(()=>{
   };
 },[]);
 
-// useEffect(()=>{
-//   console.log = function(message){
-//     const consoleOutput = document.getElementById('consoleOutput');
-//       const btn = document.querySelector('.btn1');
-//       btn.addEventListener('click',()=>{
-//         consoleOutput.innerText = "";
-//       })
-//       const paragraph = document.createElement('p');
-//       paragraph.textContent = message;
-//       consoleOutput.appendChild(paragraph);
-//       // originalConsoleLog.apply(cons5ole,arguments);
-//       originalConsoleLog.apply(console);
-//     };
-//   })
 
   const clear = ()=>{
     toast.success("Output Cleared")
@@ -133,7 +119,6 @@ useEffect(()=>{
                       <button className='vbtn'>
                       <img className='voicebtn' onClick={codeToFile}  alt='DownLoadCode'/>
                       </button>
-                        {/* <button className='btn btn1' onClick={handleSubmit}>RUN</button> */}
                         <button className='btn btn1' onClick={runCode}>RUN</button>
                       </div>
                     </div>
@@ -144,7 +129,7 @@ useEffect(()=>{
                   </div>
                   <div className='jsplayground playground'>
                     <div className='leftplayground snippet'>
-                      <textarea className='dartpython' data-testid="jsTextarea" name="javascript" id="javascript" value={code} onChange={(e)=>setcode(e.target.value)} placeholder='console.log("Hello CodoPlayer");'></textarea>
+                      <textarea className='dartpython' data-testid="jsTextarea" name="javascript" id="javascript" value={code} onChange={(e)=>setcode(e.target.value)} placeholder='console.log("Hello CodeNexus");'></textarea>
                     </div>
                     <h1 className="invisible">
                       <mark>Output</mark>
@@ -161,113 +146,3 @@ useEffect(()=>{
 }
 
 export default Javascript
-
-
-/*
-Previous code
-
-import React, { useEffect, useState } from 'react';
-import LangList from '../Editor/LangList';
-import voice from '../../assets/image.png';
-import CodeMirror from '@uiw/react-codemirror';
-import { darcula } from '@uiw/codemirror-theme-darcula';
-import { javascript } from '@codemirror/lang-javascript';
-import copy_icon from '../../assets/copy_icon.gif';
-import download_icon from '../../assets/download_logo.png';
-
-
-function Javascript() {
-
-  const [code,setcode] = useState("");
-  const [output,setOutput] = useState("");
-
-  const runCode = ()=>{
-    try {
-      const result = eval(code);
-      if (result !== undefined) {
-        setOutput(result.toString());
-      } else {
-        setOutput(output);
-      }
-    } catch (error) {
-      setOutput(error.toString());
-    }
-  };
-
-  const originalConsoleLog = console.log;
-
-  useEffect(()=>{
-    console.log = function(message){
-      const consoleOutput = document.getElementById('consoleOutput');
-      const btn = document.querySelector('.btn1');
-      btn.addEventListener('click',()=>{
-        consoleOutput.innerText = "";
-      })
-      const paragraph = document.createElement('p');
-      // const paragraph = document.querySelector(".rightplayground")
-      paragraph.textContent = message;
-
-      consoleOutput.appendChild(paragraph);
-      originalConsoleLog.apply(console,arguments);
-    };
-  },[])
-
-  const clear = ()=>{
-    const box = document.querySelector("#consoleOutput");
-    box.innerHTML = "";
-  }
-
-  return (
-    <>
-      <div className="jsContainer"> 
-            <div className="jsBody">
-                <div className="leftLang">
-                    <LangList/>
-                </div>
-                <div className="PlaygroundMain">
-                  <div className='runHeaderJS'>
-                    <div className='jsleftheaderfile jsfile'>
-                      <mark><h2>index.js</h2></mark>
-                      <div className='runbtn'>
-                      <button className='vbtn'>
-                      <img className='voicebtn' src={copy_icon} alt='voice'/>
-                      </button>
-                      <button className='vbtn'>
-                      <img className='voicebtn' src={download_icon} alt='voice'/>
-                      </button>
-                        <button className='btn btn1' onClick={runCode}>RUN</button>
-                      </div>
-                    </div>
-                    <div className='jsrightheaderfile jsfile'>
-                      <mark><p>OUTPUT</p></mark>
-                      <button className='clear' onClick={clear}>Clear</button>
-                    </div>
-                  </div>
-                  <div className='jsplayground playground'>
-                    <div className='leftplayground snippet'>
-                      <CodeMirror
-                        value={code}
-                        height='80vh'
-                        theme={darcula}
-                        // extensions={[javascript({jsx:true})]}
-                        extensions={[javascript()]}
-                        onChange={(value) =>{
-                          setcode(value)
-                        }}
-                      />
-                    </div>
-                    <div className='rightplayground snippet' id='consoleOutput' >
-                      {/* {setcode} */
-                      /* {setOutput} */
-                      /* {output} 
-                      </div>
-                      </div>
-                    </div>
-                </div>
-            </div>
-        </>
-      )
-    }
-    
-    export default Javascript
-*/

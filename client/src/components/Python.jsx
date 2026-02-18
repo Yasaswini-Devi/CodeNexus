b0a704a0db4b0a9c983e69350a9ce4a9ed6ff3c2
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2026-02-18 15:32:13 +0530

    integrated backend to frontend in python editor

--- client/src/components/Python.jsx
+++ client/src/components/Python.jsx
@@ -8,6 +8,37 @@ function Python() {
   const [code,setCode] = useState('');
   const [output, setOutput] = useState('');
 
+  const handleSubmit = async()=> {
+    toast.loading('Please Wait while File is Executing.');
+    const payload = {
+      language: "py",
+      code
+    };
+
+    try {
+      const response = await fetch("http://localhost:5000/runpy", {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body:JSON.stringify(payload)
+      })
+
+      const data = await response.json()
+      if(response.ok) {
+        toast.remove();
+        setOutput(data.output);
+        toast.success("Executed Successfully.");
+      } else {
+        setOutput(data.error);
+        toast.remove();
+        toast.error("An error occured.");
+      }
+    } catch(err) {
+      toast.remove();
+      setOutput("Error in communication with server");
+    }
+  }
 
   const clear = ()=>{
     toast.success('Output Cleared')
@@ -18,6 +49,18 @@ function Python() {
   const copyContent = ()=>{
     toast.success("Copied")
     navigator.clipboard.writeText(code);
+  }
+
+  const codeToFile = ()=>{
+    toast.success('File is Downloading...')
+    const text = document.querySelector('#python').value;
+    
+    const blob = new Blob([text],{type:"text/python"});
+
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "codofile-python.py";
+    link.click();
   }
 
   return (
@@ -32,9 +75,9 @@ function Python() {
                     <div className='jsleftheaderfile jsfile'>
                       <mark><h2>index.py</h2></mark>
                       <div className='runbtn'>
-                      <button className='vbtn'></button>
-                      <button className='vbtn'></button>
-                        <button className='btn'>RUN</button>
+                        <button className='vbtn' onClick={copyContent}>Copy</button>
+                        <button className='vbtn' onClick={codeToFile}>Download</button>
+                        <button className='btn' onClick={handleSubmit}>RUN</button>
                       </div>
                     </div>
                     <div className='jsrightheaderfile jsfile'>
@@ -44,7 +87,7 @@ function Python() {
                   </div>
                   <div className='jsplayground playground'>
                     <div className='leftplayground snippet'>
-                    <textarea className='dartpython' name="python" id="python" value={code} onChange={(e)=>setCode(e.target.value)} placeholder='print("hello codoPlayers")'></textarea>
+                    <textarea className='dartpython' name="python" id="python" value={code} onChange={(e)=>setCode(e.target.value)} placeholder='print("hello")'></textarea>
                     </div>
                     <h1 className="invisible">
                       <mark>Output</mark>

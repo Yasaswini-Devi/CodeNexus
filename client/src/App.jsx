3091aacf92517f5d3ab1ce6709a02fbbac2c4997
Shalini0810 <shalinikotha08@gmail.com>
2026-02-18 12:51:03 +0530

    added routing for javascript editor in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
+import Javascript from './components/Javascript'       
 import './App.css'
 import Python from './components/Python'
 
@@ -11,6 +12,7 @@ function App() {
       <Router>
           <Routes>
             <Route path='/' element={<LandingPage />} />
+            <Route path='/javascript' element={<Javascript />} />
             <Route path='/python' element={<Python />} />
           </Routes>
       </Router>
